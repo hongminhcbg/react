6f4e215b6e4328793272bd6108235ee3f5756f6d
Huyen Phan <huyenpt905@gmail.com>
2020-11-07 00:29:02 +0700

    update css UI

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import Board from './components/board'
+import Board from './components/board';
 
 function App() {
   return (
-    <div className="App">
-      <h1> Tic Tac Toe game </h1>
+    <div className='App'>
+      <h1 class='app-title'> Tic Tac Toe game </h1>
+      <p class='app-slogan'>An amaging game</p>
       <Board />
     </div>
   );
